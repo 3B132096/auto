2cfd70c92d759cfef1c8bb20f03eff68a803d87d
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-10-18 03:30:09 +0800

    測試monolog的使用

--- public/index.php
+++ public/index.php
@@ -14,3 +14,15 @@ $hello= new Demo\HelloWorld();
 
 use Demo\HelloWorld as World; // 類別另取別名
 $world = new World();
+
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('WISD');
+$log->pushHandler(new StreamHandler('../log/my.log', Level::Warning));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
